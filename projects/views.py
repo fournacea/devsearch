444ad76ee159007bee04d050db942722b8e4a29c
Adam Fournace <fournacea@gmail.com>
2023-10-22 11:17:28 -0400

    ADD models

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+project_list = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Sell things and stuff',
+    },
+        {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'See things I built and stuff',
+    },
+        {
+        'id': '3',
+        'title': 'social Network',
+        'description': 'Chat with friends',
+    }
+]
+
 def projects(request):
-    return render(request, 'projects/projects.html')
+    msg = "Hello!"
+    msg2 = "You are on the projects page"
+    ctx = {'message1': msg, 'message2' : msg2, 'projects': project_list}
+    return render(request, 'projects/projects.html', ctx)
 
 
 def project(request, pk):
-    ctx = {'id': str(pk)}
+    
+    project_obj = None
+    for item in project_list:
+        if item['id'] == pk:
+            project_obj = item
+
+    ctx = {'projects': project_list, 'project_obj': project_obj ,'hello': 'world'}
+
     return render(request, 'projects/single-project.html', ctx)
